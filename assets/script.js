82df86a7dbc1ec206ea23227480033d44f156b3e
UsaidAl <ali_usaid98@hotmail.com>
2024-02-20 17:54:34 +0000

    api url

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
 function getWeather() {
     const apiKey = '1b47b6fd66d703d426b5954ea6fc77f2';
     
+
+    // Get user input for the city
+  const cityInput = document.getElementById('city');
+  const city = cityInput.value;
+
+   // Construct the API URL for fetching weather data
+   const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=6`;
+
 }
