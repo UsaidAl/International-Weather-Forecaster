3b95133b80b1771d7b43807101f642535824a4fb
UsaidAl <ali_usaid98@hotmail.com>
2024-02-20 23:45:45 +0000

    forecast card and other changes to html

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,19 @@
 function getWeather() {
-    const apiKey = '1b47b6fd66d703d426b5954ea6fc77f2';
-    
+    const apiKey = '5c966b41d2f0eaebc696380cb89b7dbe';
+// Get user input for the city
+const cityInput = document.getElementById('city');
+const city = cityInput.value;
 
-    // Get user input for the city
-  const cityInput = document.getElementById('city');
-  const city = cityInput.value;
+// Construct the API URL for fetching weather data
+const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=6`;
 
-   // Construct the API URL for fetching weather data
-   const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=6`;
+// Fetch weather data from the API
+fetch(apiUrl)
+.then(function(response) {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+})
 
-    // Fetch weather data from the API
-    fetch(apiUrl)
-    .then(Response => Response.json())
-    .then(data => {
-        const weatherCard = document.getElementById('weatherCard');
-
-        if (data.error) {
-             // Display error message if there's an issue with the API request
-        weatherCard.innerHTML = `<p>${data.error.message}</p>`;
-    } else {
-      // Extract current and forecast data from the API response
-      const current = data.current;
-      const forecast = data.forecast.forecastday.slice(1); // Exclude current day
-
-      // Prepare HTML to display current weather
-      let weatherHTML = '<h2>Current Weather</h2>';
-        }
-    })
- }
+}
