5a95e67c0ce7c06b3e8e33d1e568072649c7c2f0
UsaidAl <ali_usaid98@hotmail.com>
2024-02-20 18:02:00 +0000

    Api Fetch and response

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,22 @@ function getWeather() {
    // Construct the API URL for fetching weather data
    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=6`;
 
-}
+    // Fetch weather data from the API
+    fetch(apiUrl)
+    .then(Response => Response.json())
+    .then(data => {
+        const weatherCard = document.getElementById('weatherCard');
+
+        if (data.error) {
+             // Display error message if there's an issue with the API request
+        weatherCard.innerHTML = `<p>${data.error.message}</p>`;
+    } else {
+      // Extract current and forecast data from the API response
+      const current = data.current;
+      const forecast = data.forecast.forecastday.slice(1); // Exclude current day
+
+      // Prepare HTML to display current weather
+      let weatherHTML = '<h2>Current Weather</h2>';
+        }
+    })
+ }
